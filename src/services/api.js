7b42db2489fb8ae7d33ce3e507ce65fd90220f40
luiscostalafc <luiscostalafc@gmail.com>
2020-07-16 15:42:54 -0300

    API

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://192.168.0.3:3333',
+	baseURL: 'https://jackhair-barber.com.br/',
 });
 
 export default api;
-
-// https://jackhair-barber.com.br/
