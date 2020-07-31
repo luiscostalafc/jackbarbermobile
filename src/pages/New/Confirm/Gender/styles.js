110b8df500fbbfe9c5a4f369bac9ee5529db0748
luiscostalafc <luiscostalafc@gmail.com>
2020-07-31 17:56:53 -0300

    update photos

--- src/pages/New/Confirm/Gender/styles.js
+++ src/pages/New/Confirm/Gender/styles.js
@@ -11,14 +11,15 @@ export const Container = styled.View`
 `;
 
 export const AvatarWoman = styled.Image`
-	width: 250px;
-	height: 120px;
+	margin-top: 20px;
+	width: 300px;
+	height: 200px;
 	border-radius: 20px;
 `;
 
 export const AvatarMan = styled.Image`
-	width: 250px;
-	height: 120px;
+	width: 300px;
+	height: 200px;
 	margin-top: 20px;
 	border-radius: 20px;
 `;
